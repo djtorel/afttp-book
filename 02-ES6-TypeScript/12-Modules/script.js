779d5d50b176d7d31e62024cd9affc8e20357be3
Dominic Torruellas <dtorruellas@gmail.com>
2018-03-04 10:59:42 -0700

    modules review

---
+++ 02-ES6-TypeScript/12-Modules/script.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+const utils_1 = __importDefault(require("./utils"));
+console.log(utils_1.default(2));
+utils_1.cow();
---
+++ 02-ES6-TypeScript/12-Modules/script.ts
@@ -0,0 +1,5 @@
+import square, { cow } from './utils';
+
+console.log(square(2));
+
+cow();
